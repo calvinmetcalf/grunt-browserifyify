33e8cf04ae12733e4ac667cead5a8fa4e6254381
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-09-12 22:02:19 -0400

    options

--- tasks/browserifyify.js
+++ tasks/browserifyify.js
@@ -34,30 +34,10 @@ module.exports = function(grunt) {
       tokenTo:'___forBrowserify'+Math.random()+'___'
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return rename(grunt.file.read(filepath),options.token);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+    grunt.file.write(this.option.file,rename(grunt.file.read(this.option.file),options.tokenFrom,options.tokenTo));
 
       // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+      grunt.log.writeln('File "' + f.dest + '" renamed.');
     });
   });
 
